48aff70d20a7c984c1f4787aa52979a1386e02bd
saeed <saeed.da1994@gmail.com>
2021-12-25 19:35:54 +0330

    update setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passGenerator';
+  
+  password:string='';
+  includeNum:boolean=false;
+  includeSymbol:boolean=false;
+  includeLetter:boolean=false;
+  length:number=0;
+
+
+  constructor(){}
+
+
+  onBtnClicked()
+  {
+    const numbers='0123456789';
+    const symbols='!@#$%^&*)(_+';
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    let validChar='';
+
+
+    if(this.includeNum)
+    {
+      validChar+=numbers;
+    }
+    if(this.includeLetter)
+    {
+      validChar+=letters;
+    }
+    if(this.includeSymbol)
+    {
+      validChar+=symbols;
+    }
+    
+  
+    let generatedPass='';
+    for(let index=0;index<this.length;index++)
+    {
+      let i=Math.floor(Math.random()*validChar.length);
+      generatedPass+=validChar[i];
+    }
+
+
+
+    this.password=generatedPass;
+
+
+  }
+
+  onChangeLetter()
+  {
+    this.includeLetter=!this.includeLetter
+  }
+  onChangeNum()
+  {
+    this.includeNum=!this.includeNum
+  }
+  onChangeSymbol()
+  {
+    this.includeSymbol=!this.includeSymbol
+  }
+
+  
 }
